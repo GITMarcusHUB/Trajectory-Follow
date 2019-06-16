a728fe1dae555f88c53b98941dda310c4d42e05e
Peto <peto.mark@hallgato.ppke.hu>
2019-06-16 15:25:03 +0200

    added docstrings, comments

--- agent_class.py
+++ agent_class.py
@@ -1,13 +1,23 @@
+"""
+Class of the pixel agent
+
+@author: Pető Márk
+"""
+
 
 class agent():
-	def __init__(self,
-				 posx,posy,
-				 reward,velocity):
+	def __init__(self,posx, posy, reward, velocity):
+		"""
+		:param posx: x coordinate at given time step
+		:param posy: y coordinate at given time step
+		:param reward: scalar reward in an episode
+		:param velocity: velocity at given time step
+		"""
 		self.X = posx
 		self.Y = posy
-		self.reward = reward  # reward in a given episode
+		self.reward = reward
 		self.velocity = velocity
-		# max_x: until when it get during the previous episode
+		# max_x: as far the agent gets during the previous episode
 		self.max_x = posx
 		print("Agent is created.")
 	
